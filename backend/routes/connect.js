01ec20ec5ea11b59c0f4810c35aa5becf0fcd5e4
Camille Moly <camille.moly@gmail.com>
2021-02-09 16:28:02 +0100

    refactor: delete unnecessary lines

--- backend/routes/connect.js
+++ backend/routes/connect.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
 const connectCtrl = require('../controllers/connect');
 
 router.post('/signup', connectCtrl.signup);
