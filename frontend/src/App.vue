776d99491eb4999f6ad634a007c43279e41b39e8
Camille Moly <camille.moly@gmail.com>
2021-02-22 18:45:06 +0100

    initialize vue-cli project

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav" v-if="['Home', 'About'].indexOf($route.name) > - 1"> PLACE NAVIAGTION HERE -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
