06e14d794b09cca51bb4712d0c5570948ca9d9e4
Camille Moly <camille.moly@gmail.com>
2021-02-25 20:02:36 +0100

    feat: add store states

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userId: "",
+    token: "",
+    firstName: "",
+    lastName: "",
+    profilePicture: "https://www.pngkey.com/png/full/73-730477_first-name-profile-image-placeholder-png.png",
+    about: ""
   },
   mutations: {
   },
