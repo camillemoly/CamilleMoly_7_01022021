f1793887b1b8c8e8fafbf321efa7c1cb4f41e9f5
Camille Moly <camille.moly@gmail.com>
2021-02-25 20:02:50 +0100

    feat: add routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from "../views/Login.vue"
 import Signup from "../views/Signup.vue"
+import Home from "../views/Home.vue"
+import Profile from "../views/Profile.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/login',
     name: 'Login',
@@ -21,6 +17,16 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   }
 ]
 
