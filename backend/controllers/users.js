b5f2d7be3474c06187d1b989f1e7d72b6448beec
Camille Moly <camille.moly@gmail.com>
2021-03-01 19:31:50 +0100

    fix: fix modifyUser function to add img

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,9 +15,9 @@ exports.getOneUser = (req, res, next) => {
         res.status(404).json({ error: "Aucun utilisateur ne correspond à cet identifiant !" });
       }
       const userFound = {
-        firstName: user.first_name,
-        lastName: user.last_name,
-        profilePicture: user.profile_picture,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        profile_picture: user.profile_picture,
         about: user.about
       }
       res.status(200).json(userFound);
@@ -27,26 +27,31 @@ exports.getOneUser = (req, res, next) => {
 
 // Modify a user
 exports.modifyUser = (req, res, next) => {
+    const userObject = req.file ? // TODO: fs.unlink like delete when we update a profile_picture you need to delete old picture !
+    {
+      ...JSON.parse(req.body.user),
+      profile_picture: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...JSON.parse(req.body.user) }
   models.users.findOne({ where: { id: req.params.id } })
     .then((user) => {
       if (!user) {
         return res.status(404).json({ error: "Modification impossible, cet utilisateur n'existe pas !" });
-      } else if (!req.body.first_name || !req.body.last_name) {
+      } else if (!userObject.first_name || !userObject.last_name) {
         return res.status(401).json({ error: "Les champs nom et prénom sont requis !" });
-      } else if (whitespaceValidator.test(req.body.first_name) === true || whitespaceValidator.test(req.body.last_name) === true) {
+      } else if (whitespaceValidator.test(userObject.first_name) === true || whitespaceValidator.test(userObject.last_name) === true) {
         return res.status(401).json({ error: "Les champs prénom et nom ne doivent pas contenir uniquement des espaces !" });
-      } else if (numberValidator.test(req.body.first_name) === true || numberValidator.test(req.body.last_name) === true) {
+      } else if (numberValidator.test(userObject.first_name) === true || numberValidator.test(userObject.last_name) === true) {
         return res.status(401).json({ error: "Les champs prénom et nom ne doivent pas contenir de chiffres !" });
-      } else if (symbolsValidator.test(req.body.first_name) === false || symbolsValidator.test(req.body.last_name) === false) {
+      } else if (symbolsValidator.test(userObject.first_name) === false || symbolsValidator.test(userObject.last_name) === false) {
         return res.status(401).json({ error: "Les champs prénom et nom ne doivent pas contenir de caractères spéciaux !" });
       }
       user.update({
-        ...req.body //first_name, last_name, profile_picture, about
+        ...userObject
       })
-        .then(() => res.status(201).json({ message: "Profil modifié avec succès !" }))
-        .catch((error) => res.status(500).json({ error }));
+      .then(() => res.status(201).json({ message: "Profil modifié avec succès !" }))
+      .catch((error) => res.status(500).json({ message: error.message }));
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ message: error.message }));
 };
 
 // Delete a user
@@ -56,9 +61,12 @@ exports.deleteUser = (req, res, next) => {
       if (!user) {
         return res.status(404).json({ error: "Suppression impossible, cet utilisateur n'existe pas !" });
       }
-      user.destroy()
+      const filename = user.profile_picture.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        user.destroy()
         .then(() => res.status(200).json({ message: "Utilisateur supprimé !" }))
         .catch((error) => res.status(500).json({ error }));
+      })
     })
     .catch((error) => res.status(500).json({ error }));
 };
