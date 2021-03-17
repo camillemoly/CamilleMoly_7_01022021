534af52d4cc3abad082e2ebf92641dfe7c0fce94
Camille Moly <camille.moly@gmail.com>
2021-03-17 13:49:22 +0100

    feat: add home and profile pagination

---
+++ frontend/src/components/HomePagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="getHomePosts" class="btn-secondary pagination__button">
+    <router-link :to="'/?page='+ differentPage" class="btn-secondary pagination__router-link">
+      {{ differentPage }}
+    </router-link>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "HomePagination",
+  props: ["differentPage", "getHomePosts"]
+}
+</script>
+
+<style lang="scss">
+.pagination {
+  &__button {
+    margin: 0 8px;
+    padding: 0;
+  }
+  &__router-link {
+    padding: 0 8px;
+    color: white;
+    &:hover {
+      text-decoration: none;
+      color: white;
+    }
+  }
+}
+</style>
---
+++ frontend/src/components/ProfilePagination.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="getProfilePosts" class="btn-secondary pagination__button">
+    <router-link :to="'/profile?page='+ differentPage" class="btn-secondary pagination__router-link">
+      {{ differentPage }}
+    </router-link>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ProfilePagination",
+  props: ["differentPage", "getProfilePosts"]
+}
+</script>
