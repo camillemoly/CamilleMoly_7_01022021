ba92d7f4fd4542ffe507d16163887dca2c1c20f4
Camille Moly <camille.moly@gmail.com>
2021-04-01 21:04:06 +0200

    feat: add scroll to top button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
     <router-view/>
+    <go-top style="height:60px;width:60px;border-radius:10%;background-color:#c71d78;"></go-top>
   </div>
 </template>
+
+<script>
+import GoTop from "@inotom/vue-go-top"
+export default {
+  components: {
+    GoTop
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   min-height: 100vh;
   max-width: 1440px;
   margin: 0 auto;
